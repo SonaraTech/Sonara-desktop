7bc58907d522cfadff2b57e8c4097ba322731a35
MarkushaRaz <LegoAnimation2022@gmail.com>
2025-08-30 00:37:44 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
+let Email;
+
+const Main = document.getElementById('Main');
 
 document.getElementById('Go').addEventListener('click', () => {
+    Email = document.getElementById('Email').value;
+
     fetch('http://127.0.0.1:1000/desktop/Go', {
         method:'POST',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type':'text/plain'
         },
-        body: JSON.stringify({})
-    });
+        body: Email
+    })
+        .then(response => {
+            if (response.ok) {
+                Main.innerHTML = '<div class="white"> <H2>Введите код подтверждения</H1> <H4>Присланный Вам на почту</H3> </div> <input type="text" class="center" id="Code" placeholder="Код подтверждения"> <br><br> <button id="Confirm">Подтвердить</button>';
+
+                document.getElementById('Confirm').addEventListener('click', () => {
+                    fetch('http://127.0.0.1:1000/Confirm', {
+                        method:'POST',
+                        headers: {
+                            'Content-Type':'application/json'
+                        },
+                        body: JSON.stringify({
+                            Email: Email,
+                            Code: document.getElementById('Code').value
+                        })
+                    })
+                        .then(response => {});
+                });
+            }
+            else {
+                Main.innerHTML += '<H3 class="red">Неккоректная почта</H3>';
+            }
+        });
 });
